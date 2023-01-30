84f2ea79f7b28a816c9d8f459a8350ac95c9c626
robinr2 <roeschkemis@gmail.com>
2023-01-30 16:15:59 +0100

    created object from csv data

--- src/pages/FileTableView.tsx
+++ src/pages/FileTableView.tsx
@@ -1,13 +1,49 @@
+import { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 
 type FileTableViewProps = {
   file: File | null
 }
 
+type Row = {
+  [key: string]: string
+}
+
 function FileTableView({ file }: FileTableViewProps) {
   if (file === null) return <Navigate to="/" replace />
 
-  return <h1>{file !== null ? file.name : 'No file found.'}</h1>
+  useEffect(() => {
+    const fileReader = new FileReader()
+
+    fileReader.readAsText(file)
+
+    fileReader.onload = () => {
+      if (typeof fileReader.result !== 'string') {
+        console.error(
+          'Expected datatype of result to be string but received result as ' +
+            typeof fileReader.result
+        )
+        return
+      }
+
+      const data = fileReader.result.split(/\r?\n/).map((row) => row.replace(/\"/g, '').split(';'))
+      const headers = data[0]
+      const rows: Row[] = []
+      for (let i = 1; i < data.length; i++) {
+        const row: Row = {}
+        for (let j = 0; j < headers.length; j++) {
+          row[data[0][j]] = data[i][j]
+        }
+        rows.push(row)
+      }
+    }
+
+    fileReader.onerror = () => {
+      console.error(fileReader.error)
+    }
+  }, [])
+
+  return <h1>{file.name}</h1>
 }
 
 export default FileTableView
