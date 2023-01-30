f71c3f9d248b15cce9d7fedd2eb522ce2653b1e8
robinr2 <roeschkemis@gmail.com>
2023-01-30 21:17:10 +0100

    extracted parsing of the csv into custom hook

---
+++ src/hooks/useParseCSV.ts
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'
+
+type Row = {
+  [key: string]: string
+}
+
+type HeaderTranslation = {
+  [key: string]: string
+}
+
+const HEADERS = {
+  RECIPIENT: 'recipient',
+  AMOUNT: 'amount',
+  DATE: 'date',
+  ID: 'id',
+} as const
+
+const headerTranslation: HeaderTranslation = {
+  'Beguenstigter/Zahlungspflichtiger': HEADERS.RECIPIENT,
+  Betrag: HEADERS.AMOUNT,
+  Buchungstag: HEADERS.DATE,
+  'Kontonummer/IBAN': HEADERS.ID,
+}
+
+export const useParseCSV = (file: File) => {
+  const [rows, setRows] = useState<Row[]>([])
+  const [headers, setHeaders] = useState<string[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string>()
+
+  useEffect(() => {
+    const parseCSV = (csvString: string) => {
+      const data = csvString.split(/\r?\n/).map((row) => row.replace(/\"/g, '').split(';'))
+      const translatedHeaders = data[0].map((header) => headerTranslation[header] ?? null)
+      const mappedRows: Row[] = []
+      for (let i = 1; i < data.length; i++) {
+        const row: Row = {}
+        for (let j = 0; j < translatedHeaders.length; j++) {
+          if (translatedHeaders[j] !== null) {
+            row[translatedHeaders[j]] = data[i][j]
+          }
+        }
+        mappedRows.push(row)
+      }
+
+      return { translatedHeaders, mappedRows }
+    }
+
+    const fileReader = new FileReader()
+
+    fileReader.readAsText(file)
+
+    fileReader.onload = () => {
+      if (typeof fileReader.result !== 'string') {
+        setError(
+          'Expected datatype of result to be string but received result as ' +
+            typeof fileReader.result
+        )
+        return
+      }
+
+      const { translatedHeaders, mappedRows } = parseCSV(fileReader.result)
+      setRows(mappedRows)
+      setHeaders(translatedHeaders.filter((header) => header !== null))
+      setIsLoading(false)
+    }
+
+    fileReader.onerror = () => {
+      setError(fileReader.error?.message)
+    }
+  }, [])
+
+  return { headers, rows, isLoading, error }
+}
--- src/pages/FileTableView.tsx
+++ src/pages/FileTableView.tsx
@@ -1,67 +1,16 @@
-import { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
+import { useParseCSV } from '../hooks/useParseCSV'
 
 type FileTableViewProps = {
   file: File | null
 }
 
-type Row = {
-  [key: string]: string
-}
-
-const HEADERS = {
-  RECIPIENT: 'recipient',
-  AMOUNT: 'amount',
-  DATE: 'date',
-  ID: 'id',
-} as const
-
-type HeaderTranslation = {
-  [key: string]: string
-}
-
-const headerTranslation: HeaderTranslation = {
-  'Beguenstigter/Zahlungspflichtiger': HEADERS.RECIPIENT,
-  Betrag: HEADERS.AMOUNT,
-  Buchungstag: HEADERS.DATE,
-  'Kontonummer/IBAN': HEADERS.ID,
-}
-
 function FileTableView({ file }: FileTableViewProps) {
   if (file === null) return <Navigate to="/" replace />
 
-  useEffect(() => {
-    const fileReader = new FileReader()
+  const { headers, rows, error, isLoading } = useParseCSV(file)
 
-    fileReader.readAsText(file)
-
-    fileReader.onload = () => {
-      if (typeof fileReader.result !== 'string') {
-        console.error(
-          'Expected datatype of result to be string but received result as ' +
-            typeof fileReader.result
-        )
-        return
-      }
-
-      const data = fileReader.result.split(/\r?\n/).map((row) => row.replace(/\"/g, '').split(';'))
-      const headers = data[0].map((header) => headerTranslation[header] ?? null)
-      const rows: Row[] = []
-      for (let i = 1; i < data.length; i++) {
-        const row: Row = {}
-        for (let j = 0; j < headers.length; j++) {
-          if (headers[j] !== null) {
-            row[headers[j]] = data[i][j]
-          }
-        }
-        rows.push(row)
-      }
-    }
-
-    fileReader.onerror = () => {
-      console.error(fileReader.error)
-    }
-  }, [])
+  if (isLoading) return <h1>Loading...</h1>
 
   return <h1>{file.name}</h1>
 }
