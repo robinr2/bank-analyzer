a3f07fe0b5405fd692345f2816d8399212515bb2
robinr2 <roeschkemis@gmail.com>
2023-02-03 18:06:53 +0100

    added calculation of total by recipient and by month

--- src/pages/FileAnalysis.tsx
+++ src/pages/FileAnalysis.tsx
@@ -1,21 +1,28 @@
 import { Fragment, useMemo } from 'react'
 import { useCSVDataContext } from '../context/CSVDataContext'
-import { groupRowsByMonthAndRecipient } from '../utils/groupRowsByMonthAndRecipient'
+import { groupRecipientRowsByMonths, groupRecipientTotalsByMonths } from '../utils/groupRows'
 
 function FileAnalysis() {
   const { rows } = useCSVDataContext()
 
-  const rowsGroupedByMonthAndRecipient = useMemo(() => groupRowsByMonthAndRecipient(rows), [rows])
+  const recipientRowsByMonths = useMemo(() => groupRecipientRowsByMonths(rows), [rows])
+  const recipientTotalsByMonths = useMemo(() => groupRecipientTotalsByMonths(rows), [rows])
 
   return (
     <div>
-      {Object.keys(rowsGroupedByMonthAndRecipient).map((month) => (
+      {Object.keys(recipientRowsByMonths).map((month) => (
         <Fragment key={`month-${month}`}>
-          <h2>{month}</h2>
-          {Object.keys(rowsGroupedByMonthAndRecipient[month]).map((recipient) => (
+          <h2>
+            {month}:{' '}
+            <span className="income">{recipientTotalsByMonths[month].totalIncome} EUR</span>{' '}
+            <span className="expenses">{recipientTotalsByMonths[month].totalExpenses} EUR</span>
+          </h2>
+          {Object.keys(recipientRowsByMonths[month]).map((recipient) => (
             <Fragment key={`month-${month}&recipient-${recipient}`}>
-              <h3>{recipient}</h3>
-              {rowsGroupedByMonthAndRecipient[month][recipient].map((row, rowIndex) => (
+              <h3>
+                {recipient}: {recipientTotalsByMonths[month][recipient]} EUR
+              </h3>
+              {recipientRowsByMonths[month][recipient].map((row, rowIndex) => (
                 <p key={`month-${month}&recipient-${recipient}&row-${rowIndex}`}>
                   {row.date}: {row.amount} EUR
                 </p>
---
+++ src/utils/groupRows.ts
@@ -0,0 +1,94 @@
+import { RowType } from './parseCSV'
+
+type RecipientRowsType = {
+  [key: string]: RowType[]
+}
+
+type RecipientRowsByMonthsType = {
+  [key: string]: RecipientRowsType
+}
+
+type RecipientTotalsType = {
+  totalIncome: number
+  totalExpenses: number
+  [key: string]: number
+}
+
+type RecipientTotalsByMonthsType = {
+  [key: string]: RecipientTotalsType
+}
+
+const getMonth = (date: string) => date.split('.')[1]
+
+const getMonthsAndRecipients = (rows: RowType[]) => {
+  const months = [
+    ...new Set(
+      rows.reduce((acc: string[], row) => {
+        if (!row.date) return acc
+        const month = getMonth(row.date)
+        return [month, ...acc]
+      }, [])
+    ),
+  ]
+
+  const recipients = [
+    ...new Set(
+      rows.reduce((acc: string[], row) => {
+        if (!row.recipient) return acc
+        return [row.recipient, ...acc]
+      }, [])
+    ),
+  ]
+
+  return { months, recipients }
+}
+
+const rowMatchesMonthAndRecipient = (row: RowType, month: string, recipient: string) => {
+  return row.date && getMonth(row.date) === month && row.recipient && row.recipient === recipient
+}
+
+export const groupRecipientRowsByMonths = (rows: RowType[]) => {
+  const { months, recipients } = getMonthsAndRecipients(rows)
+
+  const recipientRowsByMonths: RecipientRowsByMonthsType = {}
+  for (const month of months) {
+    const recipientRows: RecipientRowsType = {}
+    for (const recipient of recipients) {
+      for (const row of rows) {
+        if (!rowMatchesMonthAndRecipient(row, month, recipient)) continue
+        recipientRows[recipient] = recipientRows[recipient]
+          ? [row, ...recipientRows[recipient]]
+          : [row]
+      }
+    }
+    recipientRowsByMonths[month] = recipientRows
+  }
+
+  return recipientRowsByMonths
+}
+
+export const groupRecipientTotalsByMonths = (rows: RowType[]) => {
+  const { months, recipients } = getMonthsAndRecipients(rows)
+
+  const recipientTotalsByMonths: RecipientTotalsByMonthsType = {}
+  for (const month of months) {
+    const recipientTotals: RecipientTotalsType = { totalIncome: 0, totalExpenses: 0 }
+    for (const recipient of recipients) {
+      recipientTotals[recipient] = 0
+      for (const row of rows) {
+        if (rowMatchesMonthAndRecipient(row, month, recipient)) {
+          const amount = +row.amount.replace(',', '.')
+          recipientTotals[recipient] += amount
+          if (amount < 0) {
+            recipientTotals.totalExpenses += amount
+            continue
+          }
+          recipientTotals.totalIncome += amount
+        }
+      }
+    }
+    recipientTotalsByMonths[month] = recipientTotals
+  }
+
+  return recipientTotalsByMonths
+}
